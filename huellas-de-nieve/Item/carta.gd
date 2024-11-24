422ddb626c64dbd1b5d319e319b94cf0bbd57850
Ysamar_Balcazar <105230473+YsaLuna@users.noreply.github.com>
2024-11-24 09:39:28 -0500

    ultimos cambios
    ultimos arreglos finales

--- huellas-de-nieve/Item/carta.gd
+++ huellas-de-nieve/Item/carta.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 @onready var area_2d: Area2D = $Area2D
 
 func _ready():
